e7845377297e52b1f13ff45e2fa24e394f324eb8
hossam aboud <hosapoodzx@gmail.com>
2025-02-22 20:22:31 +0200

    fix_design

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', function () {
   const nav = document.querySelector('.nav_links_container');
   const mobileMenuButton = document.querySelector('.menu_mobile');
   const overlay = document.querySelector('.overlay');
+  const closeMenuButton = document.querySelector('.close_menu');
 
   // Function to close all dropdowns
   function closeAllDropdowns() {
@@ -12,6 +13,12 @@ document.addEventListener('DOMContentLoaded', function () {
       dropdown.querySelector('.dropdown_content').classList.remove('show');
       dropdown.querySelector('.rotate_arrow').classList.remove('rotate');
     });
+  }
+
+  // Function to toggle mobile menu
+  function toggleMobileMenu() {
+    nav.classList.toggle('open');
+    overlay.classList.toggle('active');
   }
 
   // Toggle dropdowns on click
@@ -27,21 +34,17 @@ document.addEventListener('DOMContentLoaded', function () {
 
   // Close dropdowns when clicking outside
   document.addEventListener('click', function (event) {
-    closeAllDropdowns();
+    if (!event.target.closest('.features_dropdown, .company_dropdown')) {
+      closeAllDropdowns();
+    }
   });
 
   // Toggle mobile menu and overlay
-  mobileMenuButton.addEventListener('click', function () {
-    nav.classList.toggle('open');
-    overlay.classList.toggle('active');
-  });
+  mobileMenuButton.addEventListener('click', toggleMobileMenu);
 
-  // Close mobile menu and overlay when clicking on overlay
-  [...document.querySelectorAll('.close_menu'), overlay].forEach((el) => {
-    el.addEventListener('click', function () {
-      nav.classList.remove('open');
-      overlay.classList.remove('active');
-    });
+  // Close mobile menu and overlay
+  [closeMenuButton, overlay].forEach((el) => {
+    el.addEventListener('click', toggleMobileMenu);
   });
 
   // Close mobile menu when a link is clicked
@@ -49,9 +52,6 @@ document.addEventListener('DOMContentLoaded', function () {
     '.nav_links_container a:not(.container_dropdown_link)'
   );
   navLinks.forEach((link) => {
-    link.addEventListener('click', function () {
-      nav.classList.remove('open');
-      overlay.classList.remove('active');
-    });
+    link.addEventListener('click', toggleMobileMenu);
   });
 });
